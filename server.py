cad13a34f36c550c40724cb93760e67df72523fa
Minh Son Tran <tranminhson1989@gmail.com>
2020-10-02 23:24:29 -0400

    Fixed ConnectionLoop and CleanClients functions

--- server.py
+++ server.py
@@ -23,26 +23,45 @@ def connectionLoop(sock):
             clients[addr] = {}
             clients[addr]['lastBeat'] = datetime.now()
             clients[addr]['color'] = 0
-            message = {"cmd": 0,"player":{"id":str(addr)}}
+
+            #--When there is a newly connected client, send a list of all the currently connected clients to the connected clients--#
+            message = {"cmd": 0,"player":[]}
+            for c in clients:
+               #message["player"].append((c[0],c[1]))
+               player = {}
+               player['id'] = str(c)
+               message['player'].append(player)
+            #message = {"cmd": 0,"player":{"id":str(addr)}}
+
             m = json.dumps(message)
             for c in clients:
                sock.sendto(bytes(m,'utf8'), (c[0],c[1]))
 
-def cleanClients():
+def cleanClients(sock):
    while True:
       for c in list(clients.keys()):
          if (datetime.now() - clients[c]['lastBeat']).total_seconds() > 5:
             print('Dropped Client: ', c)
+            
+            #--Create message to inform all current clients about the dropped player--#
+            message = {"cmd": 2,"droppedPlayer":{"id":str(c)}}
             clients_lock.acquire()
             del clients[c]
             clients_lock.release()
+
+            #--Format message to json and send to all current clients--#
+            m = json.dumps(message)
+            for c in clients:
+               sock.sendto(bytes(m,'utf8'), (c[0],c[1]))
+            #-------------------------------------------#
+
       time.sleep(1)
 
 def gameLoop(sock):
    while True:
       GameState = {"cmd": 1, "players": []}
       clients_lock.acquire()
-      print (clients)
+      print ("Current clients:",clients)
       for c in clients:
          player = {}
          clients[c]['color'] = {"R": random.random(), "G": random.random(), "B": random.random()}
@@ -62,7 +81,7 @@ def main():
    s.bind(('', port))
    start_new_thread(gameLoop, (s,))
    start_new_thread(connectionLoop, (s,))
-   start_new_thread(cleanClients,())
+   start_new_thread(cleanClients,(s,))
    while True:
       time.sleep(1)
 
